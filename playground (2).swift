e573ea71b27a42953632e6b1f1cb1878a6c5ba99
Akmal1337 <83669885+Akmal1337@users.noreply.github.com>
2021-05-04 22:16:16 +0300

    Add files via upload

---
+++ playground (2).swift
@@ -0,0 +1,15 @@
+// Решение 1 задачи 
+let a: Int = 3
+let b: Int = 1
+let c: Int = 5
+let d = ((b*b) - 4 * a * c)
+print(d)
+
+
+
+// Решение 2 задачи 
+ 
+var ab = 11 
+var bc = 2
+var d = (ab * bc ) / 2 
+print(" \(d)")
